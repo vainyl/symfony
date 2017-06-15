b46b7ec205b6d1925d324354f4e000d5eaebb793
Taras Girnyk <taras.p.gyrnik@gmail.com>
2017-06-15 17:05:35 +0300

    Added SymfonyApplicationDecorator

--- src/Application/Decorator/SymfonyApplicationDecorator.php
+++ src/Application/Decorator/SymfonyApplicationDecorator.php
@@ -10,22 +10,22 @@
  */
 declare(strict_types=1);
 
-namespace Vainyl\Symfony\Application;
+namespace Vainyl\Symfony\Application\Decorator;
 
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use Symfony\Bridge\PsrHttpMessage\HttpFoundationFactoryInterface;
 use Symfony\Bridge\PsrHttpMessage\HttpMessageFactoryInterface;
 use Symfony\Component\HttpKernel\Kernel;
-use Vainyl\Core\Application\AbstractApplication;
+use Vainyl\Core\Application\Decorator\AbstractApplicationDecorator;
 use Vainyl\Http\Application\HttpApplicationInterface;
 
 /**
- * Class SymfonyApplication
+ * Class SymfonyApplicationDecorator
  *
  * @author Taras P. Girnyk <taras.p.gyrnik@gmail.com>
  */
-class SymfonyApplication extends AbstractApplication implements HttpApplicationInterface
+class SymfonyApplicationDecorator extends AbstractApplicationDecorator
 {
     private $psrFactory;
 
@@ -36,11 +36,13 @@ class SymfonyApplication extends AbstractApplication implements HttpApplicationI
     /**
      * SymfonyApplication constructor.
      *
+     * @param HttpApplicationInterface       $httpApplication ,
      * @param HttpMessageFactoryInterface    $psrFactory
      * @param HttpFoundationFactoryInterface $symfonyFactory
      * @param Kernel                         $kernel
      */
     public function __construct(
+        HttpApplicationInterface $httpApplication,
         HttpMessageFactoryInterface $psrFactory,
         HttpFoundationFactoryInterface $symfonyFactory,
         Kernel $kernel
@@ -48,6 +50,7 @@ class SymfonyApplication extends AbstractApplication implements HttpApplicationI
         $this->psrFactory = $psrFactory;
         $this->symfonyFactory = $symfonyFactory;
         $this->appKernel = $kernel;
+        parent::__construct($httpApplication);
     }
 
     /**
---
+++ src/Kernel/AbstractSymfonyKernel.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace Vainyl\Symfony\Kernel;
+
+use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
+use Symfony\Component\HttpKernel\Kernel;
+use Vainyl\Symfony\Application\SymfonyEnvironmentInterface;
+
+/**
+ * Class AbstractSymfonyKernel
+ *
+ * @author Taras P. Girnyk <taras.p.gyrnik@gmail.com>
+ */
+abstract class AbstractSymfonyKernel extends Kernel
+{
+    private $appEnvironment;
+
+    use MicroKernelTrait;
+
+    /**
+     * AbstractSymfonyKernel constructor.
+     *
+     * @param SymfonyEnvironmentInterface $appEnvironment
+     */
+    public function __construct(SymfonyEnvironmentInterface $appEnvironment)
+    {
+        $this->appEnvironment = $appEnvironment;
+        parent::__construct($appEnvironment->getName(), $appEnvironment->isDebugEnabled());
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function registerBundles()
+    {
+        return $this->appEnvironment->getBundles();
+    }
+
+    /**
+     * @inheritDoc
+     */
+    protected function getContainerBuilder()
+    {
+        return (new \Vainyl\Symfony\Container\Factory\SymfonyContainerFactory(parent::getContainerBuilder()))->createContainer($this->appEnvironment);
+    }
+
+    /**
+     * @inheritDoc
+     */
+    protected function initializeContainer()
+    {
+        parent::initializeContainer();
+
+        $this->getContainer()->set('app.environment', $this->appEnvironment);
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function getCacheDir()
+    {
+        return $this->appEnvironment->getCacheDirectory();
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function getLogDir()
+    {
+        return $this->appEnvironment->getLogsDirectory();
+    }
+}
