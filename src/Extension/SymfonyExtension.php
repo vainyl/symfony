2f2eaa88d0d5bf49af2b456ac4e271c8b085d608
Taras Girnyk <taras.p.gyrnik@gmail.com>
2017-06-26 14:37:11 +0300

    Added AbstractBundleExtension

--- src/Extension/SymfonyExtension.php
+++ src/Extension/SymfonyExtension.php
@@ -21,11 +21,4 @@ use Vainyl\Core\Extension\AbstractFrameworkExtension;
  */
 class SymfonyExtension extends AbstractFrameworkExtension
 {
-    /**
-     * @inheritDoc
-     */
-    public function getCompilerPasses(): array
-    {
-        return [new BundleCompilerPass()];
-    }
 }
