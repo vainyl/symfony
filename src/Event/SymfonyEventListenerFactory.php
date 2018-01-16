7a619d069220351bfba680831ebd33f582879a2f
Andrey Dembitskiy <andrey.dembitskiy@cosmonova.net>
2018-01-16 12:24:53 +0200

    :sparkles: Create listener once per unique callable (for clearly remove listener)

--- src/Event/SymfonyEventListenerFactory.php
+++ src/Event/SymfonyEventListenerFactory.php
@@ -14,6 +14,7 @@ namespace Vainyl\Symfony\Event;
 
 use Symfony\Component\EventDispatcher\EventDispatcherInterface as SymfonyEventDispatcherInterface;
 use Vainyl\Core\AbstractIdentifiable;
+use Vainyl\Core\Storage\StorageInterface;
 use Vainyl\Event\EventHandlerInterface;
 
 /**
@@ -23,18 +24,51 @@ use Vainyl\Event\EventHandlerInterface;
  */
 class SymfonyEventListenerFactory extends AbstractIdentifiable implements SymfonyEventListenerFactoryInterface
 {
+    private $storage;
+
+    /**
+     * SymfonyEventListenerFactory constructor.
+     *
+     * @param StorageInterface $storage
+     */
+    public function __construct(StorageInterface $storage)
+    {
+        $this->storage = $storage;
+    }
+
     /**
      * @inheritDoc
      */
-    public function createListener(
-        $listener,
-        SymfonyEventDispatcherInterface $eventDispatcher
-    ): EventHandlerInterface {
+    public function createListener($listener, SymfonyEventDispatcherInterface $eventDispatcher): EventHandlerInterface
+    {
         $symfonyListener = $listener;
         if (is_array($symfonyListener) && isset($symfonyListener[0]) && $symfonyListener[0] instanceof \Closure) {
             $symfonyListener[0] = $listener[0]();
         }
 
-        return new SymfonyEventListenerAdapter($symfonyListener, $eventDispatcher);
+        $key = $this->mapCallableToStorageKey($symfonyListener);
+        if (false === $this->storage->offsetExists($key)) {
+            $this->storage->offsetSet(
+                $key,
+                new SymfonyEventListenerAdapter($symfonyListener, $eventDispatcher)
+            );
+        }
+
+        return $this->storage->offsetGet($key);
+    }
+
+    /**
+     * @param callable $listener
+     *
+     * @return mixed
+     */
+    private function mapCallableToStorageKey($listener)
+    {
+        $key = $listener;
+        if (is_array($key) && is_object($key[0] ?? null)) {
+            $key[0] = spl_object_hash($key[0]);
+        }
+
+        return $key;
     }
 }
